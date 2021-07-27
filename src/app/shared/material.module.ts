fdba94f2d7818fcc6265fe40675c94d709dc6248
Agata <agata.piekarczyk90@gmail.com>
2021-07-27 23:00:03 +0200

    Forms added

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -4,6 +4,9 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatStepperModule} from '@angular/material/stepper';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [],
@@ -11,7 +14,11 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatButtonModule,
     MatIconModule,
     MatCardModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatStepperModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    MatInputModule
   ]
 })
 export class MaterialModule { }
--- src/app/tips/simplicity/simplicity.component.ts
+++ src/app/tips/simplicity/simplicity.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-simplicity',
   templateUrl: './simplicity.component.html',
-  styleUrls: ['./simplicity.component.css']
+  styleUrls: ['./simplicity.component.css'],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: {
+        showError: true,
+        displayDefaultIndicatorType: false
+      }
+    }
+  ] 
 })
-export class SimplicityComponent implements OnInit {
+export class SimplicityComponent {
+    step1: FormGroup;
+    step2: FormGroup;
+    completed = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor(builder: FormBuilder)  {
+  this.step1 = builder.group(
+    {
+      isNotEmpty: [
+        '',
+        Validators.compose(
+          [Validators.required]
+        )
+      ]
+     }
+  );
+   this.step2 = builder.group(
+    {
+      isNotEmpty: [
+        '',
+        Validators.compose(
+          [Validators.required]
+        )
+      ]
+     }
+  );
+}
 
 }
+
