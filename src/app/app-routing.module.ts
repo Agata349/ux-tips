e0d982223fb63517599cf68dedd64a071347a5ce
Agata <agata.piekarczyk90@gmail.com>
2022-05-28 18:03:50 +0200

    useHash:true added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -80,13 +80,15 @@ const routes: Routes = [
   path: 'overview', component:OverviewComponent
 },
 {
-  path: '**', redirectTo: 'home'
+  path: '', redirectTo: 'home', pathMatch: 'full'
 }
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
+  imports: 
+  [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled', useHash: true})],
+
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
